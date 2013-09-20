fefbc455e2955ee65a71509c0f292cb65bee252e
maverick <maverick@ubuntu.com>
2013-09-20 01:40:14 -0700

    Subset working

--- RandomizedQueues/src/Subset.java
+++ RandomizedQueues/src/Subset.java
@@ -1,40 +1,25 @@
 import java.util.Iterator;
+import java.util.Scanner;
 
 
 public class Subset {
-	
-	
-	
+
 	public static void main(String[] args) {
 		int k = Integer.parseInt(args[0]);
-		int N = 0;
 		int count = 0;
-		String s = null;
+		String line = null;
 
-		System.out.println(k);
-		
 		RandomizedQueue<String> rq = new RandomizedQueue<String>();
 		
-		while ((s = StdIn.readString()) != null)
+		while (!StdIn.isEmpty())
 		{
-			rq.enqueue(s);
-			N++;
+			line = StdIn.readString();
+			//System.out.println(line);
+			rq.enqueue(line);
 		}
-		
-	/*	rq.enqueue("A");
-		rq.enqueue("B");
-		rq.enqueue("C");
-		rq.enqueue("D");
-		rq.enqueue("E");
-		rq.enqueue("F");
-		rq.enqueue("G");
-		rq.enqueue("H");
-		rq.enqueue("I");
-		N = 9; */
-		
-		Iterator<String> it = rq.iterator();
 
-		
+
+		Iterator<String> it = rq.iterator();
 		while((it.hasNext()) && (count < k))
 		{
 			System.out.println(it.next());
