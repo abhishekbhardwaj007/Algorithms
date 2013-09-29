a8e24248c12148c953b212157eb2cdf994f96115
maverick <maverick@ubuntu.com>
2013-09-29 02:12:30 -0700

    Tested on some big inputs

--- PatternRecognition/src/Fast.java
+++ PatternRecognition/src/Fast.java
@@ -3,44 +3,68 @@ import java.util.Arrays;
 
 public class Fast {
 
-	private static int FindFirstSlopeIndex(Point Ref, Point[] PointArray, double Slope) {
-		
-		int First = -1;
-		for (int i = 0; i < PointArray.length; i++)
+
+	private static double abs(double a) {
+		return (a <= 0.0) ? 0.0 - a : a;
+	}
+
+	private static int FindAndPrintSequence(Point P, Point[] Copy, int start)
+	{
+		double Slope = P.slopeTo(Copy[start]);
+
+		int end = start + 1;
+
+		while (end < Copy.length)
 		{
-			if (Ref.slopeTo(PointArray[i]) == Slope) {
-				First = i;
+			if ((abs(Slope -P.slopeTo(Copy[end])) <= 0.000001) || ((Slope == Double.POSITIVE_INFINITY) && (P.slopeTo(Copy[end]) == Double.POSITIVE_INFINITY)))
+			{
+				end++;
+			}
+			else
+			{
 				break;
 			}
 		}
-		
-		return First;
-	}
-	
-	private static int FindLastSlopeIndex(int First, Point Ref, Point[] PointArray, double Slope) {
-		
-		int Last = First;
-		
-		for (int i = First + 1; i < PointArray.length; i++)
+
+		if (((end - start) >= 3) && (P.compareTo(Copy[start]) == -1))
 		{
-			if (Ref.slopeTo(PointArray[i]) != Slope) {
-				Last = i - 1;
-				break;
+			System.out.print(P);
+
+			for (int k = start; k < end; k++)
+			{
+				System.out.print(" -> " + Copy[k]);
 			}
 			
-			Last = i;
+			System.out.println("");
+			
+			//
+			// Draw points
+			//
+			P.drawTo(Copy[end - 1]);
+			
+			// Indice to search from next
+			return end;
 		}
 		
-		return Last;
+		// No segment found
+		return start;
 	}
+
+	private static void PrintArray(Point[] PointArray)
+	{
+		for (int i = 0; i < PointArray.length; i++)
+		{
+			System.out.print(PointArray[i] + " ");
+		}
+
+		System.out.println("");
+	}
+
 	
 	public static void main(String[] args) {
 
 		int i, j;
-		
-		int First;
-		int Last;
-		
+
 		// Opening file stream
 		In in = new In(args[0]);
 
@@ -50,71 +74,51 @@ public class Fast {
 
 		Point[] PointArray = new Point[N];
 		Point[] Copy = new Point[N];
-		boolean[] processed = new boolean[N];
 		int index = 0;
 
 		for (int m = 1; m < A.length; m += 2) {
 			PointArray[index++] = new Point(A[m], A[m + 1]);
 		}
-		
+
+		// Get original and Copy in natural sorted order
 		Arrays.sort(PointArray);
+
+		StdDraw.setXscale(0, 32768);
+		StdDraw.setYscale(0, 32768);
 		
+		// Debugging
+		/*
+		System.out.println("Natural Sort :");
+		PrintArray(PointArray);
+		*/
+		 
+
 		for (i = 0; i < PointArray.length; i++)
 		{
 			Copy[i] = PointArray[i];
 		}
-		
-		
-		for (i = 0; i < PointArray.length; i++) {
-			
-			Point P = PointArray[i];
-			
-			Arrays.sort(Copy, P.SLOPE_ORDER);
-			
-			
-			/*
-			for (j = 0; j < Copy.length; j++) {
-				System.out.print(Copy[j]);
-				System.out.print("   " + "slope = " + P.slopeTo(Copy[j]) + " | ");
-			}
-			*/
-			
-			
-			First = FindFirstSlopeIndex(P, Copy, 0);
-			
-			if (First != -1)
-			{
-				Last = FindLastSlopeIndex(First, P, Copy, 0);
-				
-				System.out.print(P);
-				
-				for (j = First ; j <= Last; j++)
-				{
-					System.out.print(" -> " + Copy[j]);
-				}
-				
 
-				System.out.println("");
-			}
-			
-			First = FindFirstSlopeIndex(P, Copy, Double.POSITIVE_INFINITY);
-			
-			if (First != -1)
+		for (i = 0; i < PointArray.length; i++)
+		{
+			Point P = PointArray[i];
+
+			// Sort by slope order
+			Arrays.sort(Copy, P.SLOPE_ORDER);
+
+			// Debugging
+			// PrintArray(Copy);
+
+			// Keep finding subsequences whose length >= 3 and start is GREATER than reference point
+			for (j = 1; j < Copy.length; j++)
 			{
-				Last = FindLastSlopeIndex(First, P, Copy, Double.POSITIVE_INFINITY);
+				int end;
+				end = FindAndPrintSequence(P, Copy, j);
 				
-				System.out.print(P);
-				
-				for (j = First ; j <= Last; j++)
-				{
-					System.out.print(" -> " + Copy[j]);
-				}
-				
-				System.out.println("");
+				j = end;
 			}
-			
-			
+	
+			// Back to natural
+			Arrays.sort(Copy);
 		}
-        
 	}
 }
